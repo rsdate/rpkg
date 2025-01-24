370dc2b6b0bc1121f22687a9444dcce6e3cc3372
Rohan Date <rohan.s.date@icloud.com>
2025-01-23 18:19:04 -0800

    package import fixed

--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require github.com/spf13/cobra v1.8.0
 
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/rsdate/rpkgengine v0.0.0-20250124021406-65619c9a089f
 	github.com/spf13/pflag v1.0.5 // indirect
 )
--- main.go
+++ main.go
@@ -3,8 +3,12 @@ Copyright © 2024 Rohan Date <rohan.s.date@icloud.com>
 */
 package main
 
-import "github.com/rsdate/rpkg/cmd"
+import (
+	"github.com/rsdate/rpkg/cmd"
+	re "github.com/rsdate/rpkgengine"
+)
 
 func main() {
+	re.Hello()
 	cmd.Execute()
 }

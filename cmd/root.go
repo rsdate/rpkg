92f96022ef85701d302727560b22e8de43a23302
Rohan Date <rohan.s.date@icloud.com>
2025-02-20 11:49:00 -0800

    Updated long description for the rpkg comand

--- cmd/root.go
+++ cmd/root.go
@@ -17,12 +17,9 @@ var buildFile string
 var rootCmd = &cobra.Command{
 	Use:   "rpkg",
 	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long: `Rpkg is a CLI tool for building and installing packages from a remote repository.
+	For example, you can build a package by running rpkg build /path/to/project
+	You can also install a package by running rpkg install mypackage 1.0.0`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },

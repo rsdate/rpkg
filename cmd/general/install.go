dcd51491989b8fa99d8ce4cf0c1b2ca994afb4a0
Rohan Date <rohan.s.date@icloud.com>
2025-02-20 11:45:50 -0800

    Updated long description for install

--- cmd/general/install.go
+++ cmd/general/install.go
@@ -89,7 +89,10 @@ func DownloadPackage(filepath string, url string) (int, error) {
 var InstallCmd = &cobra.Command{
 	Use:   "install",
 	Short: "Installs a package from the environment variable RPKG_MIRROR",
-	Long:  ``,
+	Long: `Install installs a package from the environment variable RPKG_MIRROR.
+	For example: rpkg install mypackage 1.0.0 will install mypackage version 1.0.0 from
+	https://RPKG_MIRROR/projects/mypackage-1.0.0.tar.gz`,
+	Args: cobra.ExactArgs(2),
 	Run: func(cmd *cobra.Command, args []string) {
 		downloadPath := os.Getenv(download_dir) + "/" + args[0] + "-" + args[1] + ".tar.gz"
 		projectPath := args[0] + "-" + args[1] + ".tar.gz"
